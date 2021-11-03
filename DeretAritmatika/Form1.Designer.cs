ff2f1f4a59384d2d89afa511a64283226d6b7846
agunggumel <agung.gumelarf2011@gmail.com>
2021-11-03 00:49:35 -0700

    UploadVisualC#

---
+++ DeretAritmatika/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace DeretAritmatika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtpertama = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbanyak = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnproses = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txttengah = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btncari = new System.Windows.Forms.Button();
+            this.btndel = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtpertama
+            // 
+            this.txtpertama.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpertama.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtpertama.Location = new System.Drawing.Point(155, 114);
+            this.txtpertama.Name = "txtpertama";
+            this.txtpertama.Size = new System.Drawing.Size(168, 24);
+            this.txtpertama.TabIndex = 0;
+            this.txtpertama.TextChanged += new System.EventHandler(this.txtpertama_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Myriad Arabic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(25, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Suku Pertama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Myriad Arabic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(25, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Banyak Suku";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtbanyak
+            // 
+            this.txtbanyak.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbanyak.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtbanyak.Location = new System.Drawing.Point(155, 186);
+            this.txtbanyak.Name = "txtbanyak";
+            this.txtbanyak.Size = new System.Drawing.Size(168, 24);
+            this.txtbanyak.TabIndex = 3;
+            this.txtbanyak.TextChanged += new System.EventHandler(this.txtbanyak_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(360, 98);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(146, 160);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnproses
+            // 
+            this.btnproses.Location = new System.Drawing.Point(360, 50);
+            this.btnproses.Name = "btnproses";
+            this.btnproses.Size = new System.Drawing.Size(75, 23);
+            this.btnproses.TabIndex = 5;
+            this.btnproses.Text = "Proses";
+            this.btnproses.UseVisualStyleBackColor = true;
+            this.btnproses.Click += new System.EventHandler(this.btnproses_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Font = new System.Drawing.Font("Myriad Arabic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(23, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 66);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "INPUT";
+            // 
+            // txttengah
+            // 
+            this.txttengah.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttengah.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txttengah.Location = new System.Drawing.Point(585, 190);
+            this.txttengah.Name = "txttengah";
+            this.txttengah.Size = new System.Drawing.Size(168, 24);
+            this.txttengah.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label4.Font = new System.Drawing.Font("Myriad Arabic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(616, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 33);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Suku Tengah";
+            // 
+            // btncari
+            // 
+            this.btncari.Location = new System.Drawing.Point(632, 235);
+            this.btncari.Name = "btncari";
+            this.btncari.Size = new System.Drawing.Size(75, 23);
+            this.btncari.TabIndex = 9;
+            this.btncari.Text = "Cari";
+            this.btncari.UseVisualStyleBackColor = true;
+            this.btncari.Click += new System.EventHandler(this.btncari_Click);
+            // 
+            // btndel
+            // 
+            this.btndel.Location = new System.Drawing.Point(441, 50);
+            this.btndel.Name = "btndel";
+            this.btndel.Size = new System.Drawing.Size(75, 23);
+            this.btndel.TabIndex = 10;
+            this.btndel.Text = "Delete";
+            this.btndel.UseVisualStyleBackColor = true;
+            this.btndel.Click += new System.EventHandler(this.btndel_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label5.Font = new System.Drawing.Font("Myriad Arabic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(582, 7);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 66);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Copyright";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label6.Font = new System.Drawing.Font("Myriad Arabic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Location = new System.Drawing.Point(552, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 33);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "AgungGumelarF";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label7.Font = new System.Drawing.Font("Myriad Arabic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label7.Location = new System.Drawing.Point(691, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 33);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "0617101009";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label8.Font = new System.Drawing.Font("Myriad Arabic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label8.Location = new System.Drawing.Point(655, 98);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 33);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "IF - A";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 285);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btndel);
+            this.Controls.Add(this.btncari);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txttengah);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnproses);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtbanyak);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpertama);
+            this.Name = "Form1";
+            this.Text = "DERET ARITMATIKA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtpertama;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbanyak;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnproses;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txttengah;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btncari;
+        private System.Windows.Forms.Button btndel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ DeretAritmatika/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeretAritmatika
+{
+    public partial class Form1 : Form
+    {
+        int pertama;
+        int suku;
+        int b;
+        int[] bantu;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnproses_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            pertama = Convert.ToInt32(txtpertama.Text);
+            suku = Convert.ToInt32(txtbanyak.Text);
+
+            
+            b = 0;
+            for (int i = 1; i <= suku; i++)
+            {
+                bantu[i] = pertama * i;
+                listBox1.Items.Add(bantu[i]);
+                b = b + 1;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpertama_TextChanged(object sender, EventArgs e)
+        {
+            string tp = txtpertama.Text;
+            if (tp.Trim() == "") return;
+
+            for (int j = 0; j < tp.Length; j++)
+            {
+                if (!char.IsNumber(tp[j]))
+                {
+                    MessageBox.Show("Silakan Inputkan Angka Saja!");
+                    txtpertama.Text = "";
+                    return;
+                }
+                
+            }
+        }
+
+        private void txtbanyak_TextChanged(object sender, EventArgs e)
+        {
+            string by = txtbanyak.Text;
+            if (by.Trim() == "") return;
+
+            for (int j = 0; j < by.Length; j++)
+            {
+                if (!char.IsNumber(by[j]))
+                {
+                    MessageBox.Show("Silakan Inputkan Angka Saja!");
+                    txtbanyak.Text = "";
+                    return;
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bantu = new int[99];
+        }
+
+        private void btncari_Click(object sender, EventArgs e)
+        {
+
+            double tengah;
+            int hasil;
+
+            if (b % 2 == 1)
+            {
+                tengah = (b / 2) + 1;
+            }
+            else
+            {
+                tengah = b / 2;
+            }
+
+            hasil= Convert.ToInt32(tengah);
+
+            txttengah.Text = Convert.ToString(bantu[hasil]);
+        }
+
+        private void btndel_Click(object sender, EventArgs e)
+        {
+            txttengah.Clear();
+            txtpertama.Clear();
+            txtbanyak.Clear();
+
+            listBox1.Items.Clear();
+
+            txtpertama.Focus();
+
+        }
+    }
+}
+
